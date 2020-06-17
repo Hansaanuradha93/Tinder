099ace4f1c44a54c6c866c0e52309de0493c8a37
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-17 22:37:54 +0530

    top horizontal stack view designed

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -5,9 +5,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let redView = UIView()
-        redView.backgroundColor = .red
-        redView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        let subViews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { color -> UIView in
+            let view = UIView()
+            view.backgroundColor = color
+            return view
+        }
+        
+        let redStackView = UIStackView(arrangedSubviews: subViews)
+        redStackView.distribution = .fillEqually
+        redStackView.axis = .horizontal
+        redStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         
         let blueView = UIView()
         blueView.backgroundColor = .blue
@@ -16,7 +23,7 @@ class ViewController: UIViewController {
         yellowView.backgroundColor = .yellow
         yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
         
-        let stackView = UIStackView(arrangedSubviews: [redView, blueView, yellowView])
+        let stackView = UIStackView(arrangedSubviews: [redStackView, blueView, yellowView])
         stackView.axis = .vertical
         
         view.addSubview(stackView)
