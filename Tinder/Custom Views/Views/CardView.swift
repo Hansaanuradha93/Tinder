d30c9f906508074c504fe6d8c7ed18150d4536fc
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-18 22:50:00 +0530

    card view dragging capability added

--- Tinder/Custom Views/Views/CardView.swift
+++ Tinder/Custom Views/Views/CardView.swift
@@ -12,9 +12,32 @@ class CardView: UIView {
         
         addSubview(imageView)
         imageView.fillSuperview()
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        addGestureRecognizer(panGesture)
     }
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+// MARK: - Methods
+extension CardView {
+    
+    @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
+        
+        switch gesture.state {
+        case .changed:
+            let translation = gesture.translation(in: nil)
+            self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        
+        case .ended:
+            UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.transform = .identity
+            }, completion: nil)
+        default: ()
+        }
     }
 }
