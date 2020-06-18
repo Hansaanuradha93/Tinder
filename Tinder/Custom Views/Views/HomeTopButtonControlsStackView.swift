dbf8e1a898b9e499a969a0e79fb78c880585dc65
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-18 08:31:03 +0530

    HomeTopButtonControlStackView designed

--- Tinder/Custom Views/Views/HomeTopButtonControlsStackView.swift
+++ Tinder/Custom Views/Views/HomeTopButtonControlsStackView.swift
@@ -2,4 +2,21 @@ import UIKit
 
 class HomeTopButtonControlsStackView: UIStackView {
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        distribution = .fillEqually
+        heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let _ = [#imageLiteral(resourceName: "top_left_profile").withRenderingMode(.alwaysOriginal), #imageLiteral(resourceName: "app_icon").withRenderingMode(.alwaysOriginal), #imageLiteral(resourceName: "top_right_messages").withRenderingMode(.alwaysOriginal)].map { image -> UIView in
+            let button = UIButton(type: .system)
+            button.setImage(image, for: .normal)
+            addArrangedSubview(button)
+            return button
+        }
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -5,22 +5,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let subViews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { color -> UIView in
-            let view = UIView()
-            view.backgroundColor = color
-            return view
-        }
-        
-        let topStackView = UIStackView(arrangedSubviews: subViews)
-        topStackView.distribution = .fillEqually
-        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        let topStackView = HomeTopButtonControlsStackView()
         
         let blueView = UIView()
         blueView.backgroundColor = .blue
         
-        let buttonsStackView = HomeButtonControllsStackView()
+        let bottomStackView = HomeBottomButtonControlsStackView()
         
-        let overrallStackView = UIStackView(arrangedSubviews: [topStackView, blueView, buttonsStackView])
+        let overrallStackView = UIStackView(arrangedSubviews: [topStackView, blueView, bottomStackView])
         overrallStackView.axis = .vertical
         view.addSubview(overrallStackView)
         overrallStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
