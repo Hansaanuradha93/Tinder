a5376edbbef59d95b18d7c00ffdef6ef77a2640e
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-18 08:51:14 +0530

    padding added to TopButtonControlls

--- Tinder/Custom Views/Views/HomeTopButtonControlsStackView.swift
+++ Tinder/Custom Views/Views/HomeTopButtonControlsStackView.swift
@@ -2,17 +2,25 @@ import UIKit
 
 class HomeTopButtonControlsStackView: UIStackView {
     
+    let settingButton = UIButton(type: .system)
+    let fireButton = UIButton(type: .system)
+    let messagesButton = UIButton(type: .system)
+
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        distribution = .fillEqually
-        heightAnchor.constraint(equalToConstant: 100).isActive = true
+        distribution = .equalCentering
+        heightAnchor.constraint(equalToConstant: 80).isActive = true
+        isLayoutMarginsRelativeArrangement = true
+        layoutMargins = .init(top: 0, left: 16, bottom: 0, right: 16)
         
-        let _ = [#imageLiteral(resourceName: "top_left_profile").withRenderingMode(.alwaysOriginal), #imageLiteral(resourceName: "app_icon").withRenderingMode(.alwaysOriginal), #imageLiteral(resourceName: "top_right_messages").withRenderingMode(.alwaysOriginal)].map { image -> UIView in
-            let button = UIButton(type: .system)
-            button.setImage(image, for: .normal)
+        settingButton.setImage(#imageLiteral(resourceName: "top_left_profile").withRenderingMode(.alwaysOriginal), for: .normal)
+        fireButton.setImage(#imageLiteral(resourceName: "app_icon").withRenderingMode(.alwaysOriginal), for: .normal)
+        messagesButton.setImage(#imageLiteral(resourceName: "top_right_messages").withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        [settingButton, fireButton, messagesButton].forEach { button in
             addArrangedSubview(button)
-            return button
         }
     }
     
