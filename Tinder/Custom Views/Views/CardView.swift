acac500d7ec769e89c13dbeafb6b699cec9b80d1
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-18 22:21:43 +0530

    CardView added

---
+++ Tinder/Custom Views/Views/CardView.swift
@@ -0,0 +1,21 @@
+//
+//  CardView.swift
+//  Tinder
+//
+//  Created by Hansa Anuradha on 6/18/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
