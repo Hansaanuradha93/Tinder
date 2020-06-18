86a407af822f08185c55a13b1cedf7f74910660b
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-18 07:55:56 +0530

    HomeButtonControllsStackView added

---
+++ Tinder/Custom Views/Views/HomeButtonControllsStackView.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class HomeButtonControllsStackView: UIStackView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
