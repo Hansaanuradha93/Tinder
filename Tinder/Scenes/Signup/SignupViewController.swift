1a1bf61b02132976cf5fa63b8b1e7c2e84572ab9
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-20 22:50:36 +0530

    Signup controller added

---
+++ Tinder/Scenes/Signup/SignupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignupViewController.swift
+//  Tinder
+//
+//  Created by Hansa Anuradha on 6/20/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
