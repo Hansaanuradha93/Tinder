323fdf2345ac32850221e0decaced73a21e8d7da
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-21 00:01:00 +0530

    TDButton designed

--- Tinder/Custom Views/Buttons/TDButton.swift
+++ Tinder/Custom Views/Buttons/TDButton.swift
@@ -1,9 +1,35 @@
-//
-//  TDButton.swift
-//  Tinder
-//
-//  Created by Hansa Anuradha on 6/20/20.
-//  Copyright © 2020 Hansa Anuradha. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class TDButton: UIButton {
+
+    // MARK: Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    
+    convenience init(backgroundColor: UIColor, title: String, radius: CGFloat = 0, fontSize: CGFloat = 32) {
+        self.init(frame: .zero)
+        self.setup(backgroundColor: backgroundColor, title: title, radius: radius, fontSize: fontSize)
+    }
+}
+
+
+// MARK: - Methods
+extension TDButton {
+    
+    func setup(backgroundColor: UIColor, title: String, radius: CGFloat = 0, fontSize: CGFloat = 32) {
+        self.setTitle(title, for: .normal)
+        self.backgroundColor = backgroundColor
+        self.layer.cornerRadius = radius
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 32, weight: .heavy)
+    }
+    
+    fileprivate func configure() {
+        setTitleColor(.black, for: .normal)
+    }
+}
