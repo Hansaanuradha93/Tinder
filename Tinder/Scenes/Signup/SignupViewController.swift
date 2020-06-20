b9321ab1e7cb1702d6e624dc66560cfb7a7d8e52
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-20 23:32:46 +0530

    CustomTextFiled class introduced

--- Tinder/Scenes/Signup/SignupViewController.swift
+++ Tinder/Scenes/Signup/SignupViewController.swift
@@ -8,36 +8,48 @@ class SignupViewController: UIViewController {
         button.setTitle("Select Photo", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 32, weight: .heavy)
-        button.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 275).isActive = true
         button.layer.cornerRadius = 16
         return button
     }()
     
-    var fullNameTextField: UITextField = {
-        let textField = UITextField()
+    var fullNameTextField: CustomTextField = {
+        let textField = CustomTextField()
         textField.placeholder = "Enter full name"
         textField.backgroundColor = .white
-        textField.heightAnchor.constraint(equalToConstant: 50).isActive = true
         return textField
     }()
     
-    var emailTextField: UITextField = {
-        let textField = UITextField()
+    var emailTextField: CustomTextField = {
+        let textField = CustomTextField()
         textField.placeholder = "Enter email"
         textField.keyboardType = .emailAddress
         textField.backgroundColor = .white
-        textField.heightAnchor.constraint(equalToConstant: 50).isActive = true
         return textField
     }()
     
-    var passwordTextField: UITextField = {
-        let textField = UITextField()
+    var passwordTextField: CustomTextField = {
+        let textField = CustomTextField()
         textField.placeholder = "Enter password"
         textField.isSecureTextEntry = true
         textField.backgroundColor = .white
-        textField.heightAnchor.constraint(equalToConstant: 50).isActive = true
         return textField
     }()
+    
+    class CustomTextField: UITextField {
+        
+        override func editingRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 16, dy: 0)
+        }
+        
+        override func textRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 16, dy: 0)
+        }
+        
+        override var intrinsicContentSize: CGSize {
+            return .init(width: 0, height: 50)
+        }
+    }
     
     
     override func viewDidLoad() {
@@ -50,6 +62,6 @@ class SignupViewController: UIViewController {
         view.addSubview(stackView)
         
         stackView.centerInSuperview()
-        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 32, bottom: 0, right: 32))
+        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
     }
 }
