3fc84d8a96f6215cace68554e00b8a8eb231b3f6
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-20 23:26:22 +0530

    Signup screen designed

--- Tinder/Scenes/Signup/SignupViewController.swift
+++ Tinder/Scenes/Signup/SignupViewController.swift
@@ -2,8 +2,54 @@ import UIKit
 
 class SignupViewController: UIViewController {
 
+    var profilePhotoButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Select Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 32, weight: .heavy)
+        button.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        button.layer.cornerRadius = 16
+        return button
+    }()
+    
+    var fullNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter full name"
+        textField.backgroundColor = .white
+        textField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return textField
+    }()
+    
+    var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter email"
+        textField.keyboardType = .emailAddress
+        textField.backgroundColor = .white
+        textField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return textField
+    }()
+    
+    var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter password"
+        textField.isSecureTextEntry = true
+        textField.backgroundColor = .white
+        textField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return textField
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        
+        let stackView = UIStackView(arrangedSubviews: [profilePhotoButton, fullNameTextField, emailTextField, passwordTextField])
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        view.addSubview(stackView)
+        
+        stackView.centerInSuperview()
+        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 32, bottom: 0, right: 32))
     }
 }
