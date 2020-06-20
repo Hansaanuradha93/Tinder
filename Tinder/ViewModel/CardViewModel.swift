b5577df5dbf6e9557245df7eaf404a187499624b
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-20 22:15:55 +0530

    image changing logic added to CardViewModel

--- Tinder/ViewModel/CardViewModel.swift
+++ Tinder/ViewModel/CardViewModel.swift
@@ -5,8 +5,40 @@ protocol ProducesCardViewModel {
 }
 
 
-struct CardViewModel {
+class CardViewModel {
+    
+    // MARK: Properties
     let imageUrls: [String]
     let attributedText: NSAttributedString
     let textAlignment: NSTextAlignment
+    var imageIndexObserver: ((UIImage?) -> ())?
+    fileprivate var imageIndex = 0 {
+        didSet {
+            let imageUrl = imageUrls[imageIndex]
+            let image = UIImage(named: imageUrl)
+            imageIndexObserver?(image)
+        }
+    }
+    
+    
+    // MARK: Initializers
+    init(imageUrls: [String], attributedText: NSAttributedString, textAlignment: NSTextAlignment) {
+        self.imageUrls = imageUrls
+        self.attributedText = attributedText
+        self.textAlignment = textAlignment
+    }
+}
+
+
+// MARK: - Methods
+extension CardViewModel {
+    
+    func advanceToNextPhoto() {
+        imageIndex = min(imageIndex + 1, imageUrls.count - 1)
+    }
+    
+    
+    func goToPreviousPhot() {
+        imageIndex = max( 0, imageIndex - 1)
+    }
 }
