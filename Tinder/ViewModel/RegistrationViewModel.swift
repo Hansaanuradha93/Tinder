8d7a12d25bdd55a76a957a51687bb829c1f17266
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-21 22:04:47 +0530

    RegistrationViewModel logic added

--- Tinder/ViewModel/RegistrationViewModel.swift
+++ Tinder/ViewModel/RegistrationViewModel.swift
@@ -2,4 +2,21 @@ import UIKit
 
 class RegistrationViewModel {
     
+    var fullName: String? { didSet { checkFormValidity() } }
+    var email: String? { didSet { checkFormValidity() } }
+    var password: String? { didSet { checkFormValidity() } }
+    
+    // MARK: Reactive Programming
+    var isFormValidObserver: ((Bool) -> ())?
+}
+
+
+// MARK: - Methods
+extension RegistrationViewModel {
+    
+    fileprivate func checkFormValidity() {
+        
+        let isFormValid = fullName?.isEmpty == false && email?.isEmpty == false && password?.isEmpty == false
+        isFormValidObserver?(isFormValid)
+    }
 }
