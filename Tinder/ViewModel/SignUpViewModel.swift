ab76cfc225e75f797e9f02a0e56af712cc5641d8
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-22 10:14:15 +0530

    Bindlable integrated to SignupViewModel

--- Tinder/ViewModel/SignUpViewModel.swift
+++ Tinder/ViewModel/SignUpViewModel.swift
@@ -2,14 +2,14 @@ import UIKit
 
 class SignUpViewModel {
     
-    var image: UIImage? { didSet { imageObserver?(image) } }
     var fullName: String? { didSet { checkFormValidity() } }
     var email: String? { didSet { checkFormValidity() } }
     var password: String? { didSet { checkFormValidity() } }
     
-    // MARK: Reactive Programming
-    var isFormValidObserver: ((Bool) -> ())?
-    var imageObserver: ((UIImage?) -> ())?
+    // MARK: Bindlable
+    var bindableImage = Bindalbe<UIImage>()
+    var bindalbeIsFormValid = Bindalbe<Bool>()
+    
 }
 
 
@@ -17,8 +17,7 @@ class SignUpViewModel {
 extension SignUpViewModel {
     
     fileprivate func checkFormValidity() {
-        
         let isFormValid = fullName?.isEmpty == false && email?.isEmpty == false && password?.isEmpty == false
-        isFormValidObserver?(isFormValid)
+        bindalbeIsFormValid.value = isFormValid
     }
 }
