98a033b13ed8f2f0f076bfbd273626b2db5ed9c3
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-23 10:21:08 +0530

    download image fro url extension added

---
+++ Tinder/Extensions/UIImageView+Ext.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIImageView {
+    
+    func downloadImage(from url: String) {
+        NetworkManager.shared.downloadImage(from: url) { [weak self] image in
+            guard let self = self else { return }
+            DispatchQueue.main.async { self.image = image }
+        }
+    }
+}
