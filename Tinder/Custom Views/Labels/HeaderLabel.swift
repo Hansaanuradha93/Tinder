01e65865079bbc8c42a43a544fa19e754c0925a6
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-24 22:18:31 +0530

    HeaderLabel added

---
+++ Tinder/Custom Views/Labels/HeaderLabel.swift
@@ -0,0 +1,21 @@
+//
+//  HeaderLabel.swift
+//  Tinder
+//
+//  Created by Hansa Anuradha on 6/24/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class HeaderLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
