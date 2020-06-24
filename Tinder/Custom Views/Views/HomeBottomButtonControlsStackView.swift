594b36205027f408cb28dd63d86531e21fc7bcf5
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-24 09:05:35 +0530

    HomeBottomButtonStackView code refactored and cleaned

--- Tinder/Custom Views/Views/HomeBottomButtonControlsStackView.swift
+++ Tinder/Custom Views/Views/HomeBottomButtonControlsStackView.swift
@@ -2,21 +2,41 @@ import UIKit
 
 class HomeBottomButtonControlsStackView: UIStackView {
 
+    // MARK: Properties
+    let refreshButton = createButton(image: #imageLiteral(resourceName: "refresh_circle").withRenderingMode(.alwaysOriginal))
+    let dislikeButton = createButton(image: #imageLiteral(resourceName: "dismiss_circle").withRenderingMode(.alwaysOriginal))
+    let superLikeButton = createButton(image: #imageLiteral(resourceName: "super_like_circle").withRenderingMode(.alwaysOriginal))
+    let likeButton = createButton(image:  #imageLiteral(resourceName: "like_circle").withRenderingMode(.alwaysOriginal))
+    let thunderButton = createButton(image: #imageLiteral(resourceName: "boost_circle").withRenderingMode(.alwaysOriginal))
+
+    
     // MARK: Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        distribution = .fillEqually
-        heightAnchor.constraint(equalToConstant: 100).isActive = true
-        
-        let _ = [#imageLiteral(resourceName: "refresh_circle").withRenderingMode(.alwaysOriginal), #imageLiteral(resourceName: "dismiss_circle").withRenderingMode(.alwaysOriginal), #imageLiteral(resourceName: "super_like_circle").withRenderingMode(.alwaysOriginal), #imageLiteral(resourceName: "like_circle").withRenderingMode(.alwaysOriginal), #imageLiteral(resourceName: "boost_circle").withRenderingMode(.alwaysOriginal)].map { image -> UIView in
-            let button = UIButton(type: .system)
-            button.setImage(image, for: .normal)
-            addArrangedSubview(button)
-            return button
-        }
+        setupLayout()
     }
     
     
     required init(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
+
+
+// MARK: - Methods
+extension HomeBottomButtonControlsStackView {
+    
+    static func createButton(image: UIImage) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setImage(image, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFill
+        return button
+    }
+    
+    
+    fileprivate func setupLayout() {
+        distribution = .fillEqually
+        heightAnchor.constraint(equalToConstant: 100).isActive = true
+        [refreshButton, dislikeButton, superLikeButton, likeButton, thunderButton].forEach { button in
+            addArrangedSubview(button)
+        }
+    }
 }
