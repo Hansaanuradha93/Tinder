4fe886cd7472c815767bd0e7ec9032ef1d4f3a6b
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-28 22:16:31 +0530

    Scene delegate -> Code cleared

--- Tinder/SceneDelegate.swift
+++ Tinder/SceneDelegate.swift
@@ -12,15 +12,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         self.window = window
         window.makeKeyAndVisible()
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {}
-
-    func sceneDidBecomeActive(_ scene: UIScene) {}
-
-    func sceneWillResignActive(_ scene: UIScene) {}
-
-    func sceneWillEnterForeground(_ scene: UIScene) {}
-
-    func sceneDidEnterBackground(_ scene: UIScene) {}
 }
 
