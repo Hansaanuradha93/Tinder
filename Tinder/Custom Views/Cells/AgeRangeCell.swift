9ac99025bf832d33a3373720d51a75f639740002
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-29 09:28:25 +0530

    AgeRangeCell basic configurations

--- Tinder/Custom Views/Cells/AgeRangeCell.swift
+++ Tinder/Custom Views/Cells/AgeRangeCell.swift
@@ -2,4 +2,16 @@ import UIKit
 
 class AgeRangeCell: UITableViewCell {
     
+    // MARK: Properties
+    static let reuseIdentifier = "AgeRangeCell"
+    
+    
+    // MARK: Initializers
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .red
+    }
+    
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 }
