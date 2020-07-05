0e5bec627d7e4c0946a1c81945e4ccdccd2a5d66
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-05 10:54:28 +0530

    SwipingPhotosViewController basic configurations

--- Tinder/Scenes/User Details/Custom Views/SwipingPhotosViewController.swift
+++ Tinder/Scenes/User Details/Custom Views/SwipingPhotosViewController.swift
@@ -4,5 +4,35 @@ class SwipingPhotosViewController: UIPageViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dataSource = self
+
+        view.backgroundColor = .white
+        
+        let redViewController = UIViewController()
+        redViewController.view.backgroundColor = .red
+        
+        let controller = [redViewController]
+        
+        setViewControllers(controller, direction: .forward, animated: false)
     }
+}
+
+
+// MARK: -
+extension SwipingPhotosViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let greenViewController = UIViewController()
+        greenViewController.view.backgroundColor = .green
+        return greenViewController
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let yellowViewController = UIViewController()
+        yellowViewController.view.backgroundColor = .yellow
+        return yellowViewController
+    }
+    
+    
 }
