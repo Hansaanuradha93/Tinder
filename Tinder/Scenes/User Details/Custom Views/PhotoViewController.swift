ae87f57a61127a8acb06510cbfac3c6d9adbe718
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-05 10:53:38 +0530

    PhotoViewController added

---
+++ Tinder/Scenes/User Details/Custom Views/PhotoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoViewController.swift
+//  Tinder
+//
+//  Created by Hansa Anuradha on 7/5/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
