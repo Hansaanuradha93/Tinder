8c558b4c1cd21f252e7744cdbf74d06d67586acf
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-06 17:42:02 +0530

    default user age constant added

--- Tinder/Contants/Constants.swift
+++ Tinder/Contants/Constants.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 struct Constants {
+    static let defaultUserAge = 18
     static let defaultMinimumSeekingAge = 18
-    static let defaultMaximumSeekingAge = 18
+    static let defaultMaximumSeekingAge = 60
 }
