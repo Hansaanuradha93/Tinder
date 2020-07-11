5ffad6e7955439a7ed5000ce630c20bdef0804ac
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-11 10:28:56 +0530

    MassageViewController added

---
+++ Tinder/Scenes/Message/MessageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MessageViewController.swift
+//  Tinder
+//
+//  Created by Hansa Anuradha on 7/11/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
