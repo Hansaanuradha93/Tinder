6ce4e3654e653551d0e24c99479ba2d037e28fce
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-11 20:46:11 +0530

    MessageCell added

---
+++ Tinder/Scenes/Message/Cells/MessageCell.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class MessageCell: UICollectionViewCell {
+    
+    static let reuseID = "MessageCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
