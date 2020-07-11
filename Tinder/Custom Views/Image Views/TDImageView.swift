87d27fa2dd12512b53a10469ee2446c84581f8ca
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-11 14:12:57 +0530

    TDImageView optimised

--- Tinder/Custom Views/Image Views/TDImageView.swift
+++ Tinder/Custom Views/Image Views/TDImageView.swift
@@ -5,28 +5,18 @@ class TDImageView: UIImageView {
     // MARK: Initialisers
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupUI()
     }
     
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
     
-    convenience init(image: UIImage = Asserts.placeHolder, borderWidth: CGFloat = 0, borderColor: UIColor = .clear) {
+    convenience init(image: UIImage = Asserts.placeHolder, borderWidth: CGFloat = 0, borderColor: UIColor = .clear, contentMode: ContentMode = .scaleAspectFill) {
         self.init(frame: .zero)
         self.image = image
         self.layer.borderWidth = borderWidth
         self.layer.borderColor = borderColor.cgColor
-    }
-}
-
-
-// MARK: - Methods
-extension TDImageView {
-    
-    fileprivate func setupUI() {
-        image = Asserts.placeHolder
-        contentMode = .scaleAspectFill
-        clipsToBounds = true
+        self.contentMode = contentMode
+        self.clipsToBounds = true
     }
 }
