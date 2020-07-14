86a689f61bb9b8abd8d7f0f30fe39a82538d5400
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-14 22:47:10 +0530

    Message model changed

--- Tinder/Model/Message.swift
+++ Tinder/Model/Message.swift
@@ -4,15 +4,18 @@ struct Message {
     
     // MARK: Properties
     let text: String?
+    let isFromCurrentUser: Bool?
     
     
     // MARK: Initializers
-    init(text: String) {
+    init(text: String, isFromCurrentUser: Bool) {
         self.text = text
+        self.isFromCurrentUser = isFromCurrentUser
     }
     
     
     init(dictionary: [String : Any]) {
         self.text = dictionary["text"] as? String
+        self.isFromCurrentUser = dictionary["isFromCurrentUser"] as? Bool
     }
 }
