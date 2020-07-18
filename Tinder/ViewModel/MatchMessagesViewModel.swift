de0ab30a8903d832f2b053a475bd528ec1987b08
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-18 22:58:32 +0530

    MatchesViewModel added

--- Tinder/ViewModel/MatchMessagesViewModel.swift
+++ Tinder/ViewModel/MatchMessagesViewModel.swift
@@ -1,22 +1,4 @@
 import Foundation
 import Firebase
 
-class MatchMessagesViewModel {
-    
-    func fetchMatches(completion: @escaping (Match?) -> ()) {
-        guard let currentUserID = Auth.auth().currentUser?.uid else { return }
-        let reference = Firestore.firestore().collection("matches_messages").document(currentUserID).collection("matches")
-        reference.getDocuments { (querySnapshot, error) in
-            if let error = error {
-                print(error.localizedDescription)
-                completion(nil)
-                return
-            }
-            querySnapshot?.documents.forEach({ (documentSnapshot) in
-                let dictionary = documentSnapshot.data()
-                let match = Match(dictionary: dictionary)
-                completion(match)
-            })
-        }
-    }
-}
+class MatchMessagesViewModel {}
