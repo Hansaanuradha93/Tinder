bcefc17f676a7cf66a741c3307254f4929eaa36a
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-18 22:33:45 +0530

    MatchesViewController added

---
+++ Tinder/Scenes/Match Messages/MatchesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MatchesViewController.swift
+//  Tinder
+//
+//  Created by Hansa Anuradha on 7/18/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class MatchesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
