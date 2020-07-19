70daba24f5409c9ae652016efed2634ab3c42fc0
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-19 15:35:13 +0530

    match message ui tweaked

--- Tinder/Scenes/Match Messages/MatchMessagesViewController.swift
+++ Tinder/Scenes/Match Messages/MatchMessagesViewController.swift
@@ -49,7 +49,7 @@ extension MatchMessagesViewController: UICollectionViewDelegateFlowLayout {
     
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        return UIEdgeInsets(top: 16, left: 16, bottom: 0, right: 16)
     }
     
     
