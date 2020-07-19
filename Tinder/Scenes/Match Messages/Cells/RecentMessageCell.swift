1ebc7c62890d2e23435c7bf138d913077af2e59d
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-19 12:55:05 +0530

    RecentMessageCell added and populated

---
+++ Tinder/Scenes/Match Messages/Cells/RecentMessageCell.swift
@@ -0,0 +1,13 @@
+//
+//  RecentMessageCell.swift
+//  Tinder
+//
+//  Created by Hansa Anuradha on 7/19/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class RecentMessageCell: UICollectionViewCell {
+    
+}
