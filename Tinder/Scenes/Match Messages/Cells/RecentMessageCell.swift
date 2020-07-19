99f0daa72c5e956260225071371c128a3c16b1c9
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-19 15:03:17 +0530

    Recent message cell designed

--- Tinder/Scenes/Match Messages/Cells/RecentMessageCell.swift
+++ Tinder/Scenes/Match Messages/Cells/RecentMessageCell.swift
@@ -5,6 +5,10 @@ class RecentMessageCell: UICollectionViewCell {
     // MARK: Properties
     static let reuseID = "RecentMessageCell"
     
+    fileprivate let profileImageView = TDImageView()
+    fileprivate let usernameLabel = TDLabel(text: "Username here", textAlignment: .left, fontSize: 16)
+    fileprivate let messageLabel = TDLabel(text: "This is really a long sentense since i want to check if a long text will filt in", textAlignment: .left, textColor: .gray, fontSize: 14, numberOfLines: 2)
+    fileprivate let barView = UIView()
     
     // MARK: Initializers
     override init(frame: CGRect) {
@@ -21,6 +25,24 @@ class RecentMessageCell: UICollectionViewCell {
 extension RecentMessageCell {
     
     fileprivate func setupUI() {
-        backgroundColor = .red
+        let dimensions: CGFloat = 100
+        let padding = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 20)
+        backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.layer.cornerRadius = dimensions / 2
+        profileImageView.alignLeadingInSuperView(leading: leadingAnchor, paddingLeading: 16)
+        profileImageView.centerVerticallyInSuperView(size: .init(width: dimensions, height: dimensions))
+        
+        let stackView = UIStackView(arrangedSubviews: [usernameLabel, messageLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        addSubview(stackView)
+        stackView.anchor(top: nil, leading: profileImageView.trailingAnchor, bottom: nil, trailing: trailingAnchor, padding: padding)
+        stackView.centerVerticallyInSuperView()
+        
+        barView.backgroundColor = UIColor.appColor(color: .regularGray)
+        addSubview(barView)
+        barView.anchor(top: nil, leading: profileImageView.trailingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: padding, size: .init(width: 0, height: 1))
     }
 }
