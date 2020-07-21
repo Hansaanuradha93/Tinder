b4d2c2c3e1491c863c79f306302c8668fec66d14
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-21 23:07:12 +0530

    RecentMessage model changed

--- Tinder/Model/RecentMessage.swift
+++ Tinder/Model/RecentMessage.swift
@@ -1,6 +1,18 @@
 import Firebase
 
 struct RecentMessage {
+    
+    // MARK: Properties
     let text, uid, name, profileImageUrl: String?
-    let timestamp: Timestamp
+    let timestamp: Timestamp?
+    
+    
+    // MARK: Initializers
+    init(dictionary: [String : Any]) {
+        self.text = dictionary["text"] as? String
+        self.uid = dictionary["uid"] as? String
+        self.name = dictionary["name"] as? String
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String
+        self.timestamp = dictionary["timestamp"] as? Timestamp
+    }
 }
