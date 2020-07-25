2964774654d7dde3bf14b6310b69adf4e86291ff
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-25 11:30:39 +0530

    MatchesViewModel code refactored

--- Tinder/ViewModel/MatchesViewModel.swift
+++ Tinder/ViewModel/MatchesViewModel.swift
@@ -1,20 +1,40 @@
 import Firebase
 
 class MatchesViewModel {
-    func fetchMatches(completion: @escaping (Match?) -> ()) {
+    
+    // MARK: Properties
+    fileprivate var matches = [Match]()
+}
+
+
+// MARK: - Methods
+extension MatchesViewModel {
+    
+    func getMatchesCount() -> Int {
+        return matches.count
+    }
+    
+    
+    func getMatchAt(_ indexPath: IndexPath) -> Match {
+        return matches[indexPath.item]
+    }
+    
+    
+    func fetchMatches(completion: @escaping (Bool) -> ()) {
         guard let currentUserID = Auth.auth().currentUser?.uid else { return }
         let reference = Firestore.firestore().collection("matches_messages").document(currentUserID).collection("matches")
         reference.getDocuments { (querySnapshot, error) in
             if let error = error {
                 print(error.localizedDescription)
-                completion(nil)
+                completion(false)
                 return
             }
             querySnapshot?.documents.forEach({ (documentSnapshot) in
                 let dictionary = documentSnapshot.data()
                 let match = Match(dictionary: dictionary)
-                completion(match)
+                self.matches.append(match)
             })
+            completion(true)
         }
     }
 }
