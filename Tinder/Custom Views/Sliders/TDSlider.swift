39da9e5aaf1cce1c293a11cb3e6c9e6c5f05aaa5
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-26 20:08:27 +0530

    TDSlider added

---
+++ Tinder/Custom Views/Sliders/TDSlider.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+class TDSlider: UISlider {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    
+    required init?(coder: NSCoder) { fatalError() }
+   
+    
+    override func trackRect(forBounds bounds: CGRect) -> CGRect {
+        
+        let customBounds = CGRect(origin: bounds.origin, size: CGSize(width: bounds.size.width, height: 5.0))
+        super.trackRect(forBounds: customBounds)
+        return customBounds
+    }
+}
+
+
+// MARK: - Methods
+extension TDSlider {
+    
+    fileprivate func setupUI() {
+        minimumValue = Float(Constants.defaultMinimumSeekingAge)
+        maximumValue = Float(Constants.defaultMaximumSeekingAge)
+        isContinuous = true
+        layer.cornerRadius = 30
+        tintColor = UIColor.appColor(color: .pink)
+        setThumbImage(Asserts.sliderThumb, for: .normal)
+        setThumbImage(Asserts.sliderThumb, for: .highlighted)
+    }
+}
+
