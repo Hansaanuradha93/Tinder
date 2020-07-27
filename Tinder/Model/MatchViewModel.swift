a3e972349b69c74ec2bbcaecd3a1634eeffb979d
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-27 21:46:09 +0530

    MatchViewModel added

---
+++ Tinder/Model/MatchViewModel.swift
@@ -0,0 +1,17 @@
+import Firebase
+
+class MatchViewModel {
+    
+    func fetchCardUser(cardUID: String, completion: @escaping (User?) -> ()) {
+        let reference = Firestore.firestore().collection("users").document(cardUID)
+        reference.getDocument { snapshot, error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+                return
+            }
+            guard let dictionary = snapshot?.data() else { return }
+            completion(User(dictionary: dictionary))
+        }
+    }
+}
