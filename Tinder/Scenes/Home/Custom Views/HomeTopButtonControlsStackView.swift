b8142f5f122a295befbb7ff2dd5003e1798e9f1b
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-07-27 21:32:55 +0530

    HomeTopButtonControlsStackView refactored

--- Tinder/Scenes/Home/Custom Views/HomeTopButtonControlsStackView.swift
+++ Tinder/Scenes/Home/Custom Views/HomeTopButtonControlsStackView.swift
@@ -3,29 +3,36 @@ import UIKit
 class HomeTopButtonControlsStackView: UIStackView {
     
     // MARK: Properties
-    let settingsButton = UIButton(type: .system)
-    let fireButton = UIButton(type: .system)
-    let messagesButton = UIButton(type: .system)
+    let settingsButton = TDButton(type: .system)
+    let fireButton = TDButton(type: .system)
+    let messagesButton = TDButton(type: .system)
     
     
     // MARK: Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        setupUI()
+    }
+    
+    
+    required init(coder: NSCoder) { fatalError() }
+}
+
+
+// MARK: - Methods
+extension HomeTopButtonControlsStackView {
+    fileprivate func setupUI() {
         distribution = .equalCentering
         heightAnchor.constraint(equalToConstant: 80).isActive = true
         isLayoutMarginsRelativeArrangement = true
         layoutMargins = .init(top: 0, left: 16, bottom: 0, right: 16)
         
-        settingsButton.setImage(#imageLiteral(resourceName: "top_left_profile").withRenderingMode(.alwaysOriginal), for: .normal)
-        fireButton.setImage(#imageLiteral(resourceName: "app_icon").withRenderingMode(.alwaysOriginal), for: .normal)
-        messagesButton.setImage(#imageLiteral(resourceName: "top_right_messages").withRenderingMode(.alwaysOriginal), for: .normal)
+        settingsButton.setImage(Asserts.profile.withRenderingMode(.alwaysOriginal), for: .normal)
+        fireButton.setImage(Asserts.fireIcon.withRenderingMode(.alwaysOriginal), for: .normal)
+        messagesButton.setImage(Asserts.topMessagesRight.withRenderingMode(.alwaysOriginal), for: .normal)
         
         [settingsButton, fireButton, messagesButton].forEach { button in
             addArrangedSubview(button)
         }
     }
-    
-    
-    required init(coder: NSCoder) { fatalError() }
 }
