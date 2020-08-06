0c2fbcb1827b301a0e0eee81163ace59c49903bc
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-06 21:11:56 +0530

    Adviser model bug fixed and code refactored

--- Tinder/Model/Advertiser.swift
+++ Tinder/Model/Advertiser.swift
@@ -1,9 +1,7 @@
 import UIKit
 
 struct Advertiser {
-    let title: String
-    let brandName: String
-    let posterImageUrl: String
+    let title, brandName, posterImageUrl: String?
 }
 
 
@@ -11,8 +9,8 @@ struct Advertiser {
 extension Advertiser: ProducesCardViewModel {
     
     func toCardViewModel() -> CardViewModel {
-        let attributedText = NSMutableAttributedString(string: title, attributes: [.font: UIFont.systemFont(ofSize: 34, weight: .heavy)])
-        attributedText.append(NSAttributedString(string: "\n\(brandName)", attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .semibold)]))
-        return CardViewModel(uid: "", imageUrls: [posterImageUrl], attributedText: attributedText, textAlignment: .center)
+        let attributedText = NSMutableAttributedString(string: title ?? "", attributes: [.font: UIFont.systemFont(ofSize: 34, weight: .heavy)])
+        attributedText.append(NSAttributedString(string: "\n\(String(describing: brandName))", attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .semibold)]))
+        return CardViewModel(uid: "", imageUrls: [posterImageUrl ?? ""], attributedText: attributedText, textAlignment: .center)
     }
 }
