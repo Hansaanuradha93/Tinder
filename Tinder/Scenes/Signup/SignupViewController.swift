1594987790e616b37abef9f9613dd331a5ceb1c3
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-02-07 20:46:27 +0530

    SignupViewController code cleaned

--- Tinder/Scenes/Signup/SignupViewController.swift
+++ Tinder/Scenes/Signup/SignupViewController.swift
@@ -3,15 +3,15 @@ import UIKit
 class SignupViewController: UIViewController {
     
     // MARK: Properties
-    fileprivate let gradientLayer = CAGradientLayer()
-    fileprivate let profilePhotoButton = TDButton(backgroundColor: .white, title: Strings.selectPhoto, radius: 16, fontSize: 32)
-    fileprivate let fullNameTextField = TDTextField(padding: 16, placeholderText: Strings.enterFullName, radius: 25)
-    fileprivate let emailTextField = TDTextField(padding: 16, placeholderText: Strings.enterEmail, radius: 25)
-    fileprivate let passwordTextField = TDTextField(padding: 16, placeholderText: Strings.enterPassword, radius: 25)
-    fileprivate let signupButton = TDButton(backgroundColor: UIColor.appColor(color: .lightGray), title: Strings.signup, titleColor: .gray, radius: 25, fontSize: 24)
-    fileprivate let goToLoginButton = TDButton(backgroundColor: .clear, title: Strings.goToLogin, titleColor: .white, radius: 0, fontSize: 18)
+    private let gradientLayer = CAGradientLayer()
+    private let profilePhotoButton = TDButton(backgroundColor: .white, title: Strings.selectPhoto, radius: 16, fontSize: 32)
+    private let fullNameTextField = TDTextField(padding: 16, placeholderText: Strings.enterFullName, radius: 25)
+    private let emailTextField = TDTextField(padding: 16, placeholderText: Strings.enterEmail, radius: 25)
+    private let passwordTextField = TDTextField(padding: 16, placeholderText: Strings.enterPassword, radius: 25)
+    private let signupButton = TDButton(backgroundColor: UIColor.appColor(color: .lightGray), title: Strings.signup, titleColor: .gray, radius: 25, fontSize: 24)
+    private let goToLoginButton = TDButton(backgroundColor: .clear, title: Strings.goToLogin, titleColor: .white, radius: 0, fontSize: 18)
     
-    fileprivate lazy var verticalStackView: UIStackView = {
+    private lazy var verticalStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [fullNameTextField, emailTextField, passwordTextField, signupButton])
         stackView.axis = .vertical
         stackView.distribution = .fillEqually
@@ -19,8 +19,8 @@ class SignupViewController: UIViewController {
         return stackView
     }()
     
-    fileprivate lazy var overrallStackView = UIStackView(arrangedSubviews: [profilePhotoButton, verticalStackView])
-    fileprivate let signupViewModel = SignUpViewModel()
+    private lazy var overrallStackView = UIStackView(arrangedSubviews: [profilePhotoButton, verticalStackView])
+    private let signupViewModel = SignUpViewModel()
 
     
     // MARK: View Controller
@@ -59,9 +59,9 @@ class SignupViewController: UIViewController {
 
 
 // MARK: - Objc Methods
-extension SignupViewController {
+private extension SignupViewController {
     
-    @objc fileprivate func handleSignUp() {
+    @objc func handleSignUp() {
         handleTapDismiss()
         signupViewModel.performSignUp { [weak self] error in
             guard let self = self else { return }
@@ -74,21 +74,21 @@ extension SignupViewController {
     }
     
     
-    @objc fileprivate func handleTextChange(textField: UITextField) {
+    @objc func handleTextChange(textField: UITextField) {
         signupViewModel.fullName = fullNameTextField.text
         signupViewModel.email = emailTextField.text
         signupViewModel.password = passwordTextField.text
     }
     
     
-    @objc fileprivate func handleKeyboardHide() {
+    @objc func handleKeyboardHide() {
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
             self.view.transform = .identity
         })
     }
     
     
-    @objc fileprivate func handleKeyboardShow(notification: Notification) {
+    @objc func handleKeyboardShow(notification: Notification) {
         guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
         let keyboardFrame = value.cgRectValue
         let bottomSpace = view.frame.height - overrallStackView.frame.origin.y - overrallStackView.frame.height
@@ -97,18 +97,18 @@ extension SignupViewController {
     }
     
     
-    @objc fileprivate func handleTapDismiss() {
+    @objc func handleTapDismiss() {
         view.endEditing(true)
     }
     
     
-    @objc fileprivate func handleGoToLogin() {
+    @objc func handleGoToLogin() {
         let loginController = LoginViewController()
         navigationController?.pushViewController(loginController, animated: true)
     }
     
     
-    @objc fileprivate func handleSelectPhoto() {
+    @objc func handleSelectPhoto() {
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
         imagePickerController.allowsEditing = true
@@ -118,16 +118,16 @@ extension SignupViewController {
 
 
 // MARK: - Methods
-extension SignupViewController {
+private extension SignupViewController {
     
-    fileprivate func navigateToHome() {
+    func navigateToHome() {
         let navController = UINavigationController(rootViewController: HomeViewController())
         navController.modalPresentationStyle = .fullScreen
         present(navController, animated: true)
     }
     
     
-    fileprivate func setupRegistrationViewModelObserver() {
+    func setupRegistrationViewModelObserver() {
         signupViewModel.bindalbeIsFormValid.bind { [weak self] isFormValid in
             guard let self = self, let isFormValid = isFormValid else { return }
             if isFormValid {
@@ -156,31 +156,31 @@ extension SignupViewController {
     }
     
     
-    fileprivate func handleLandscapeOrientation() {
+    func handleLandscapeOrientation() {
         overrallStackView.axis = .horizontal
         profilePhotoButton.widthAnchor.constraint(equalToConstant: 275).isActive = true
     }
     
     
-    fileprivate func handlePortraitOrientation() {
+    func handlePortraitOrientation() {
         overrallStackView.axis = .vertical
         profilePhotoButton.heightAnchor.constraint(equalToConstant: 275).isActive = true
     }
     
     
-    fileprivate func handleTapGesture() {
+    func handleTapGesture() {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss))
         view.addGestureRecognizer(tapGesture)
     }
     
     
-    fileprivate func setupNotifications() {
+    func setupNotifications() {
         NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     
-    fileprivate func layoutUI() {
+    func layoutUI() {
         navigationController?.navigationBar.isHidden = true
         profilePhotoButton.heightAnchor.constraint(equalToConstant: 275).isActive = true
         emailTextField.keyboardType = .emailAddress
@@ -199,7 +199,7 @@ extension SignupViewController {
     }
     
     
-    fileprivate func addTargets() {
+    func addTargets() {
         profilePhotoButton.addTarget(self, action: #selector(handleSelectPhoto), for: .touchUpInside)
         fullNameTextField.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
         emailTextField.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
@@ -209,7 +209,7 @@ extension SignupViewController {
     }
     
     
-    fileprivate func setupGradient() {
+    func setupGradient() {
         gradientLayer.colors = [UIColor.appColor(color: .orange).cgColor, UIColor.appColor(color: .pink).cgColor]
         gradientLayer.locations = [0, 1]
         view.layer.insertSublayer(gradientLayer, at: 0)
